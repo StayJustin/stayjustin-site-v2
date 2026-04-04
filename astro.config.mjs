62b210a56ed10231725a56eb6d8cf899a4d9e117
StayJustin <ThisJustInH@outlook.com>
2026-04-04 14:56:14 -0700

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://stayjustin.com',
-  integrations: [tailwind()],
+  site: 'https://stayjustin.com', // 👈 THIS is the key line
+  integrations: [tailwind(), sitemap()],
 });
