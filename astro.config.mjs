8fb2a2499f70dd4ae08c2a12dd72fcfe51bd917f
StayJustin <ThisJustInH@outlook.com>
2026-04-04 15:16:29 -0700

    Remove sitemap integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://stayjustin.com', // 👈 THIS is the key line
-  integrations: [tailwind(), sitemap()],
+  site: 'https://stayjustin.com',
+  integrations: [tailwind()],
 });
