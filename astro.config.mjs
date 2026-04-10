99b940bea90997fcf8fa6b69a7e1a9037e59e2c0
StayJustin <ThisJustInH@outlook.com>
2026-04-09 22:15:14 -0700

    Update site URL and change integration to sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://stayjustin.com',
-  integrations: [tailwind()],
+  site: 'https://www.stayjustin.com',
+  integrations: [sitemap()],
 });
