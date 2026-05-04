68842646aa522ff04142471f7d25e77532591d6f
StayJustin <ThisJustInH@outlook.com>
2026-05-04 05:58:59 -0700

    Add SpeedInsights integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
+import SpeedInsights from "@vercel/speed-insights/astro"
 
 export default defineConfig({
   site: 'https://www.stayjustin.com',
