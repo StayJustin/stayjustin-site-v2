cfd8cb7390dc6889870c7011dd9696ef4bf9e124
StayJustin <ThisJustInH@outlook.com>
2026-05-04 11:22:26 -0700

    Remove SpeedInsights integration from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import SpeedInsights from "@vercel/speed-insights/astro"
 
 export default defineConfig({
   site: 'https://www.stayjustin.com',
